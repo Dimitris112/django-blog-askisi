e8b1e4e017cb23e4d8516c220deb835feff80494
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-07-09 17:06:21 +0000

    evala arketa docstrings (oxi se ola) se py files, ekana kai collect static kai migrations

--- blog/views.py
+++ blog/views.py
@@ -23,6 +23,10 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`.
 
     **Template:**
 
@@ -60,7 +64,16 @@ def post_detail(request, slug):
 
 def comment_edit(request, slug, comment_id):
     """
-    View to edit comments
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+    An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`.
     """
     if request.method == "POST":
 
@@ -83,7 +96,14 @@ def comment_edit(request, slug, comment_id):
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
